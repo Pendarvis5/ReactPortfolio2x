fcab932c28bde1fe13c76945d7372854e578437f
Nicole Marie <nicolependarvis5@gmail.com>
2023-04-26 23:30:02 -0400

    starter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from "./logo.svg";
+import header from "./components/header/header";
+import navbar from "./components/navbar/navbar";
+import about from "./components/about/about";
+import section from "./components/section/section";
+import portfolio from "./components/portfolio/portfolio";
+import footer from "./components/footer/footer";
+import contact from "./components/contact/contact";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header />
+      <navbar />
+      <about />
+      <section />
+      <portfolio />
+      <footer />
+      <contact />
+    </>
   );
-}
+};
 
 export default App;
--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const footer = () => {
+const portfolio = () => {
   return <div></div>;
 };
 
-export default footer;
+export default portfolio;
